59acfe1b7d530656e248b5519353ce79f81a4b7d
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-28 14:54:47 -0300

    testando inserção de dois comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    test("teste para inserção de dois comentarios", () => {
+        render (<PostComment />)
+        fireEvent.change(screen.getByTestId("campo-comentario"), {
+            target: {
+                value: "Incrivel"
+            }
+        })
+
+        fireEvent.click(screen.getByTestId("btn-comentar"),)
+
+        fireEvent.change(screen.getByTestId("campo-comentario"), {
+            target: {
+                value: "Gostaria de comprar"
+            }
+        })
+
+        fireEvent.click(screen.getByTestId("btn-comentar"),)
+
+        expect(screen.getByText("Incrivel")).toBeInTheDocument()
+        expect(screen.getByText("Gostaria de comprar")).toBeInTheDocument()
+    })
 });
